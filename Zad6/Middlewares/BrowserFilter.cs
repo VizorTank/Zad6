0ae3cc8a8ab74ce16ecab2acced9ce3ea1dc2816
VizorTank <36300779+VizorTank@users.noreply.github.com>
2021-04-21 22:43:43 +0200

    Dodaj pliki projektów.

---
+++ Zad6/Middlewares/BrowserFilter.cs
@@ -0,0 +1,33 @@
+﻿using BrowserNET;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zad6.Middlewares
+{
+    public class BrowserFilter
+    {
+        private RequestDelegate _next;
+        public BrowserFilter(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var parser = new UserAgentParser(context.Request.Headers["User-Agent"]);
+            parser.Determine();
+
+            var browserName = parser.BrowserName;
+
+            var forbiddenBrowsers = new List<string> { "Edge", "Internet Explorer", "EdgeChromium" };
+
+            if (forbiddenBrowsers.Any(browserName.Contains))
+                await context.Response.WriteAsync("Przeglądarka nie jest obsługiwana");
+            else
+                await _next(context);
+        }
+    }
+}
---
+++ Zad6/Middlewares/UseBrowserFilterExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zad6.Middlewares
+{
+    public static class UseBrowserFilterExtension
+    {
+        public static IApplicationBuilder UseBrowserFilter(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<BrowserFilter>();
+        }
+    }
+}
